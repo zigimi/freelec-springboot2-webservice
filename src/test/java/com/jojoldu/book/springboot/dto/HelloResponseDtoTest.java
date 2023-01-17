55c4881da9d85105d56c6db621f02d5fd40d4f31
zigimi <chju0905@pusan.ac.kr>
2023-01-17 22:57:35 +0900

    2장

---
+++ src/test/java/com/jojoldu/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,17 @@
+package com.jojoldu.book.springboot.dto;
+
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬북_기능_테스트() {
+        String name = "test";
+        int amount = 100;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
