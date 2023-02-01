12ad104901d0756e96a9d634e0437c507cf88968
zigimi <99969990+zigimi@users.noreply.github.com>
2023-02-01 10:30:03 +0900

    Update IndexContollerTest.java

--- src/test/java/com/jojoldu/book/springboot/web/IndexContollerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/IndexContollerTest.java
@@ -21,6 +21,6 @@ public class IndexContollerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스 Ver.actions");
     }
 }
