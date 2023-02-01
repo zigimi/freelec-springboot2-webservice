d80f8e41d56f2c72d8c891d4e71aabe467663f96
zigimi <99969990+zigimi@users.noreply.github.com>
2023-02-01 11:11:10 +0900

    Update Application.java

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(args); 
+        SpringApplication.run(Application.class, args); 
     }
 }
